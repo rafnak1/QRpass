a3ea590701205e8157399c1641a5b99b0df66e8f
DamaralHenrique <rique01101999@gmail.com>
2020-04-21 16:45:58 -0300

    :tada: Initial commit

---
+++ back-end/functions/src/index.ts
@@ -0,0 +1,26 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp()
+
+ // Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+ export const helloWorld = functions.https.onRequest((request, response) => {
+    console.log('Aqui tu pode escrever mensagens pro log visto apenas no firebase! Mas n ta funcionando... Funcionou, lembre de sempre salvar antes de dar deploy :)')
+  response.send("Hello from me!");
+  //GetGuest;
+ });
+
+ export const GetGuest = functions.https.onRequest((request, response) => {
+  const promise = admin.firestore().doc('Convidado/IZIwkK6GnseQJ5OGSjdY').get()
+  const p2 = promise.then(snapshot => {
+      const data = snapshot.data()
+      response.send(data)
+  })
+  p2.catch(error => {
+      //handle the error
+      console.log(error)
+      response.status(500).send(error)
+  })
+
+});
