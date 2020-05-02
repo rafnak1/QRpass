6083d290248e54d69ae38800701a2485604d1e6f
alberto caeiro <rafa.nakata@gmail.com>
2020-05-02 01:43:49 -0300

    leitor 1.0

---
+++ leitor/leitor.py
@@ -0,0 +1,102 @@
+from picamera import PiCamera
+from pyzbar.pyzbar import decode
+from PIL import Image
+from os import system
+from time import time
+from time import sleep
+
+gui_app = 'feh'
+kiosk_switch = '-F -Z'
+server_url = 'https://ilhabela-c33df.web.app/?qr='
+path = '/home/pi/proj/'
+
+no_usr_match = 'no-usr-match.png'
+no_event_match = 'no-event-match.png'
+event_found = 'event-locked.png'
+off_sync = 'off-sync.png'
+
+#The following are small functionalities. ----------
+
+#Returns the server's answer (in bytes) from the "response" file.
+def readResponse():
+    global path
+    with open(path + 'response', 'rb') as resp:
+        response = resp.read()
+    return response
+
+def curlToServer(payload):
+    global server_url
+    global path
+    system('curl ' + server_url + payload + ' -o ' + path + 'response')
+
+def refreshWindow(new_window):
+    global gui_app
+
+    with open('window', 'w') as order:
+        order.write(new_window)
+
+    system('wmctrl -c ' + gui_app)
+
+#The following are logical functionalities. ------------
+
+def preSync():
+    
+    curlToServer(QR[0].data)
+            
+    if readResponse() == b'no match':
+        refreshWindow(no_event_match)
+
+    else:
+        refreshWindow(event_found)
+        sync_QR = QR[0].data
+        synced = True
+
+
+def postSync():
+    if QR[0].data == sync_QR and cycle_count > 0:
+        system('shutdown now')
+    else:   
+        #send qr data in GET request; let's see if it's a valid user QR...
+        curlToServer(QR[0].data) 
+
+        if readResponse == b'no match':
+            refreshWindow(no_usr_match)
+
+        else:   #if response isn't "no match", then it must have found a picture.
+            refreshWindow(response)
+
+#main-------------
+
+sleep(25)
+
+refreshWindow(off_sync)
+
+sync_QR = b'0'
+synced = False
+
+#prepare camera
+camera = PiCamera()
+camera.resolution = (1024, 768)
+
+cycle_count = 0
+
+while True:
+    t0 = time()
+    
+    while time() - t0 < 30:
+        
+        camera.capture('rbpic.jpg')
+        QR = decode(Image.open(path + 'rbpic.jpg'))
+ 
+        if len(QR) != 0:
+        
+            if not synced:
+                preSync()
+            
+            else:
+                postSync()
+    
+    #refreshes window every 30 seconds
+    system('wmctrl -c' + gui_app)
+
+    cycle_count += 1
